8ce6b552a796128101fe69df85f85da70cdc9e39
matheusbalbino1 <matheusbalbinodeoliveira3@gmail.com>
2022-03-19 16:25:56 -0300

    att

--- indexAssets/pagamentosAssets/scripts.js
+++ indexAssets/pagamentosAssets/scripts.js
@@ -5,26 +5,26 @@ let elementos = {
     botaoFinalizar: document.getElementById("botaoFinalizar")
 }
 
-elementos.botaoCep.addEventListener("click", conferirCep)
+elementos.botaoCep.addEventListener("click", pararEvento)
 
 elementos.botaoImprimir.addEventListener("click", pararEvento)
 
 elementos.botaoFinalizar.addEventListener("click", finalizar)
 
-function conferirCep(){
+// function conferirCep(){
 
-    let valorInput = elementos.inputCep.value;
+//     let valorInput = elementos.inputCep.value;
 
-    if(valorInput.length !== 8 ){
-        elementos.inputCep.value = ""
-        alert("CEP Incorreto")
-        return
-    }
+//     if(valorInput.length !== 8 ){
+//         elementos.inputCep.value = ""
+//         alert("CEP Incorreto")
+//         return
+//     }
 
-    valorInput = valorInput.substr(0, 5) + "-" + valorInput.substr(5, 7)
-    elementos.inputCep.value = valorInput;
+//     valorInput = valorInput.substr(0, 5) + "-" + valorInput.substr(5, 7)
+//     elementos.inputCep.value = valorInput;
 
-}
+// }
 
 function pararEvento(){
     event.preventDefault()
