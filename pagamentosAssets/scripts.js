9c72cd3ced5a8b80e8a1db7c47c4c3e89be342f2
matheusbalbino1 <matheusbalbinodeoliveira3@gmail.com>
2022-03-18 23:48:04 -0300

    att

--- pagamentosAssets/scripts.js
+++ pagamentosAssets/scripts.js
@@ -1,8 +1,8 @@
 let elementos = {
     botaoCep: document.getElementById("botaoCep"),
     inputCep: document.getElementById("inputCep"),
-    botaoImprimir: document.getElementsByClassName("botaoImprimir")[0],
-    botaoFinalizar: document.getElementsByClassName("botaoFinalizar")[0]
+    botaoImprimir: document.getElementById("botaoImprimir"),
+    botaoFinalizar: document.getElementById("botaoFinalizar")
 }
 
 elementos.botaoCep.addEventListener("click", conferirCep)
@@ -12,6 +12,7 @@ elementos.botaoImprimir.addEventListener("click", pararEvento)
 elementos.botaoFinalizar.addEventListener("click", finalizar)
 
 function conferirCep(){
+
     let valorInput = elementos.inputCep.value;
 
     if(valorInput.length !== 8 ){
@@ -20,13 +21,11 @@ function conferirCep(){
         return
     }
 
-    
-
     valorInput = valorInput.substr(0, 5) + "-" + valorInput.substr(5, 7)
     elementos.inputCep.value = valorInput;
 
-  
 }
+
 function pararEvento(){
     event.preventDefault()
     return
